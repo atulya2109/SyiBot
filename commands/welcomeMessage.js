e702e8ad59d7e4aec7f019ecc7e6f165e5341bd4
Atulya Bist <atulya2109@gmail.com>
2020-05-10 14:05:55 +0530

    Added Files

---
+++ commands/welcomeMessage.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'welcomemsg',
+    description: 'Sends a message to a specific channel when a user joins',
+    args: true,
+    usage: 'set <channel> \nunset <channel>',
+    guildOnly: true,
+    execute(message,args){        
+
+    },
+};
--- index.js
+++ index.js
@@ -1,19 +1,45 @@
+const fs = require('fs')
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {prefix,token} = require('./config.json')
 
+const {prefix} = require('./config.json');
+const token = process.env.API_KEY;
+
+
+client.commands = new Discord.Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name,command);
+    console.info(`Command Set: ${command.name}`);
+}
 
 client.on('ready',() => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.info(`Logged in as ${client.user.tag}!`);
 })
 
 client.on('message', msg => {
     
-    if(msg.content.startsWith(prefix) || msg.author.bot) return;
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
 
     const args = msg.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
-    
+    const commandName = args.shift().toLowerCase();
+
+    if(!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    try{
+        command.execute(msg,args);
+    }
+    catch(error){
+        
+        console.error(`Failed To Execute ${commandName}`);
+        msg.channel.send('There was an error while executing that command');
+    }
+
 });
 
-client.login(token)
+client.login(token);
